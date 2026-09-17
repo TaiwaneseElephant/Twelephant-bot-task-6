d1ceddcc72b009168332f5b5576ab79529785891
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-18 02:27:24 +0800

    Update JSON-CSD.js

--- JSON-CSD.js
+++ JSON-CSD.js
@@ -1,6 +1,6 @@
-async function search(query) {
-    const response = await fetch(`https://zh.wikipedia.org/w/api.php?action=query&list=search&srnamespace=*&srprop=snippet&srsearch=${query}&formatversion=2`, {
-        header: {"user-agent": "Twelephant-bot"}
+async function search(query_string) {
+    const response = await fetch(`https://zh.wikipedia.org/w/api.php?action=query&list=search&srnamespace=*&srprop=snippet&srsearch=${query_string}&formatversion=2`, {
+        headers: {userAgent: "Twelephant-bot"}
     }
     if (!response.ok) {
         throw new Error(response.status);
@@ -13,4 +13,23 @@ async function search(query) {
     return result; 
 }
 
-query = 'insource:/\"_addText\": \"\{\{\s*(((db?|c?sd|speedy|delete|[速快][刪删])\s*\|)|db-).*?}}/i contentmodel:json'
+async function getConfig() {
+    const response = await fetch("https://zh.wikipedia.org/w/index.php?title=User:Twelephant-bot/task/6/config.json&action=raw&ctype=application/json", {
+        headers: {userAgent: "Twelephant-bot"}
+    }
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    const config = await response.json();
+    if (!config.Enable) {
+        return;
+    }
+    const query_string = config.query_string;
+    const item = config.item;
+    const data = await search(query_string);
+    const content = ";
+    for (let page of data.query.search) {
+      content.push([page.title, page.snippet.match(/<span class=\"searchmatch\">.+</span>/)]);
+    }
+}
+
