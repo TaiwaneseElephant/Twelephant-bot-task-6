405f9b43702d6a9b51dcac67f0a77213604557c0
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-19 23:28:26 +0800

    Fix syntax error in fetch call in JSON-CSD.js

--- JSON-CSD.js
+++ JSON-CSD.js
@@ -1,7 +1,7 @@
 async function search(query_string) {
     const response = await fetch(`https://zh.wikipedia.org/w/api.php?action=query&list=search&srnamespace=*&srprop=snippet&srsearch=${query_string}&formatversion=2`, {
         headers: {userAgent: "Twelephant-bot"}
-    }
+    });
     if (!response.ok) {
         throw new Error(response.status);
     }
