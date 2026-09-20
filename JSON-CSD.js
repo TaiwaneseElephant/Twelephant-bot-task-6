92bfce8d043bf2b81291adcd216679e69cde68eb
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-20 23:47:29 +0800

    Log JSON response in JSON-CSD.js

--- JSON-CSD.js
+++ JSON-CSD.js
@@ -81,6 +81,7 @@ async function login(name, pwd, headers, cookies) {
     if (!response.ok) {
         throw new Error(response.status);
     }
+    config.log(await response.json());
     return setHeaders(response, cookies);
 }
 
