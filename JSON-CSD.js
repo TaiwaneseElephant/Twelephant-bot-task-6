3c06da1a143cb56fa73434b77c4f6f752fb19caf
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-20 22:13:18 +0800

    Log headers in search function

--- JSON-CSD.js
+++ JSON-CSD.js
@@ -2,6 +2,7 @@ async function search(query_string, headers) {
     const response = await fetch(`https://zh.wikipedia.org/w/api.php?action=query&list=search&srnamespace=*&srprop=snippet&srsearch=${encodeURIComponent(query_string)}&formatversion=2&format=json`, {
         headers: headers
     });
+    console.log(headers);
     if (!response.ok) {
         throw new Error(response.status);
     }
